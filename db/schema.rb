fbfda8aecec5d10803e27ec893fea222050028e4
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2023-07-06 14:06:32 +0200

    create table/model client

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_06_120211) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_06_120556) do
+  create_table "clients", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "post_code"
+    t.string "city"
+    t.integer "siret_number"
+    t.string "tva_number"
+    t.string "phone_number"
+    t.string "email"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_clients_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -29,4 +44,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_06_120211) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "clients", "users"
 end
