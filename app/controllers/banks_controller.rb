6fc0393de73ce61cdfe8d5eedadbde496b7ae300
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2023-07-09 20:28:18 +0200

    add methods in banks controller

--- app/controllers/banks_controller.rb
+++ app/controllers/banks_controller.rb
@@ -1,2 +1,35 @@
 class BanksController < ApplicationController
+  def new
+    @bank = Bank.new
+  end
+
+  def create
+    @bank = Bank.new(bank_params)
+    @bank.user = current_user
+    if @bank.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @bank = Bank.find(params[:id])
+  end
+
+  def update
+    @bank = Bank.find(params[:id])
+    @bank.update(bank_params)
+  end
+
+  def destroy
+    @bank = Bank.find(params[:id])
+    @bank.destroy
+  end
+
+  private
+
+  def bank_params
+    params.require(:bank).permit(:name, :iban_number, :bic_number)
+  end
 end
