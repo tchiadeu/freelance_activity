b94b2aac7c88c1a90e1d64acadff0df0016699b8
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2023-07-10 09:28:04 +0200

    pages organisation with navbar logic

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,16 @@ class PagesController < ApplicationController
   before_action :authenticate_user!
 
   def dashboard
-    @bills = Bill.where(user: current_user)
+  end
+
+  def clients
     @clients = Client.where(user: current_user)
+  end
+
+  def administrative
+    @bills = Bill.where(user: current_user)
+  end
+
+  def profile
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'pages#dashboard'
+  get 'clients', to: 'pages#clients'
+  get 'administrative', to: 'pages#administrative'
+  get 'profile', to: 'pages#profile'
   resources 'bills', only: %i[show new create edit update destroy]
   resources 'clients', only: %i[show new create edit update destroy]
   resources 'banks', only: %i[new create edit update destroy]
