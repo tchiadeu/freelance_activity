2b322d5544b1e940f04c26eb84101036758116e7
kevin tchiadeu <kevin.tchiadeu@gmail.com>
2023-07-20 11:22:33 +0200

    change the navbar redirection to clients adding an index method to clients controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'pages#dashboard'
-  get 'clients', to: 'pages#clients'
   get 'administrative', to: 'pages#administrative'
   get 'profile', to: 'pages#profile'
+  resources 'clients'
   resources 'bills', only: %i[show new create edit update destroy]
-  resources 'clients', only: %i[show new create edit update destroy]
   resources 'banks', only: %i[new create edit update destroy]
 end
